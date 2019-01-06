f547e97e15073210726da378696403b7e0efc492
Eldar <sahaanonimus10@gmail.com>
2019-01-06 11:59:23 +0600

    tasks

--- nodejs/learnyounode/11prg.js
+++ nodejs/learnyounode/11prg.js
@@ -1,10 +1,11 @@
 var http = require('http');
 var fs = require('fs');
 var port = process.argv[2];
-var file = fs.createReadStream(process.argv[3]);
+var file = process.argv[3];
 
 var server = http.createServer(function(request,response){
-console.log(file)
+file = fs.createReadStream(file);
+var exit = file.pipe(response);
 });
 
 server.listen(port);
