7e8917c6d8aac6077f992db1e853d7cb16929876
Eldar <sahaanonimus10@gmail.com>
2019-01-11 22:53:00 +0600

    tasks

--- nodejs/expressworks/5/5prg.js
+++ nodejs/expressworks/5/5prg.js
@@ -1,10 +1,7 @@
 var express = require('express');
     app = express();
+    app.use(require('stylus').middleware(process.argv[3]));
     port = process.argv[2];
-    // files = process.agrv[3];
 
-app.get('/',function(req,res){
-        app.use(express.static( || path.join(__dirname, '5')));
-});
 
 app.listen(port);
