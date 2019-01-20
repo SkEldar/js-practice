abd15db449042eb50349e8701f7dc229afb99886
Eldar <sahaanonimus10@gmail.com>
2019-01-20 22:55:25 +0600

    tasks

--- full-stack/script.js
+++ full-stack/script.js
@@ -2,30 +2,30 @@ $(document).ready(function(){
     $.get(
         "http://localhost:5000/books",
         function(data) {
-            console.log(data.obj1);
-            console.log(data.obj2);
-            console.log(data.obj3);
-            var name1 = document.getElementById('name1');
-            var name2 = document.getElementById('name2');
-            var name3 = document.getElementById('name3');
+            console.log(data);
+            // console.log(data.obj2);
+            // console.log(data.obj3);
+            // var name1 = document.getElementById('name1');
+            // var name2 = document.getElementById('name2');
+            // var name3 = document.getElementById('name3');
 
-            var tags1 = document.getElementById('tags1');
-            var tags2 = document.getElementById('tags2');
-            var tags3 = document.getElementById('tags3');
+            // var tags1 = document.getElementById('tags1');
+            // var tags2 = document.getElementById('tags2');
+            // var tags3 = document.getElementById('tags3');
 
-            var url1 = document.getElementById('url1');
-            var url2 = document.getElementById('url2');
+            // var url1 = document.getElementById('url1');
+            // var url2 = document.getElementById('url2');
 
-            name1.innerHTML = data.obj1.title;
-            name2.innerHTML = data.obj2.title;
-            name3.innerHTML = data.obj3.title;
+            // name1.innerHTML = data.obj1.title;
+            // name2.innerHTML = data.obj2.title;
+            // name3.innerHTML = data.obj3.title;
 
-            tags1.innerHTML = data.obj1.tags;
-            tags2.innerHTML = data.obj2.tags;
-            tags3.innerHTML = data.obj3.tags;
+            // tags1.innerHTML = data.obj1.tags;
+            // tags2.innerHTML = data.obj2.tags;
+            // tags3.innerHTML = data.obj3.tags;
 
-            url1.innerHTML = data.obj1.url;
-            url2.innerHTML = data.obj2.url;
+            // url1.innerHTML = data.obj1.url;
+            // url2.innerHTML = data.obj2.url;
         }
     );
 });
--- full-stack/server.js
+++ full-stack/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 const cors = require('cors');
+var mongo = require('mongodb').MongoClient;
+var globalDB;
     app = express();
     obj = {
         "obj1" : { title: 'Express.js Guide',
@@ -11,13 +13,25 @@ const cors = require('cors');
         "obj3" :  { title: 'JavaScript: The Good Parts', tags: [ 'javascript' ] }
     };
 
-
-    // data = JSON.parse(data);
-    // res.json(data);
-
+mongo.connect('mongodb://localhost:27017/books',cors(),function(err,db){
+   globalDB = db;
+        if(err) return console.log(err);
+        globalDB.collection('books').insert(obj,function(err,data){
+            if(err) return console.log(err);    
+            console.log(data);
+        });
+});
 app.get('/books',cors(),function(req,res){
-    // obj = JSON.parse(obj);
-     res.json(obj);
+        globalDB.collection('books').find().toArray(function(err,docs){
+            if(err) return console.log(err);
+             var obj1 = docs[0].obj1;
+             var obj2 = docs[0].obj2;
+             var obj3 = docs[0].obj3;
+            //  console.log(obj1)
+            for(var i = 0; i < 3; i++) {
+                res.json(obj[i]);
+            };
+                });
 });
 
-app.listen(5000);
+app.listen(5000)
