12f200453a29197549179a813b8859a493764bb7
Eldar <sahaanonimus10@gmail.com>
2019-02-05 23:03:57 +0600

    tasks

--- appreact/src/components/Article.js
+++ appreact/src/components/Article.js
@@ -1,15 +1,31 @@
-import React from "react"
+import React,{Component} from "react"
 
-function Article (props) {
-    const {article} = props
-    const body = <section>{article.text}</section>
+class Article extends Component {
+    state = {
+        isOpen : true
+    }
+         handleClick = () => {
+            console.log(1);
+            this.setState({
+                isOpen : !this.state.isOpen
+            })
+        }
+
+    render() {
+        const {article} = this.props
+
+    const body = this.state.isOpen && <section>{article.text}</section>
     return (
         <div>
-            <h2>{article.title}</h2>
+            <h2>{article.title}
+            <button onClick={this.handleClick}>close</button>
+            </h2>
+
             {body}
             <h3>creation date : {(new Date(article.date)).toDateString()}</h3>
         </div>
     )
-};
+    }
+}
 
 export default Article
