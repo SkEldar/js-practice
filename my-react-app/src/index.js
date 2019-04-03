4cbe98ffc5d9e6278df6c47716e243c7fb6f5d39
Eldar <sahaanonimus10@gmail.com>
2019-04-03 22:26:21 +0600

    tasks

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import ReactDOM,{render} from 'react-dom';
+import {render} from 'react-dom';
 
 
 
@@ -9,23 +9,38 @@ class Button extends Component{
     constructor(props){
         super(props);
         this.state = {
-             count : 3
+             count : 0
         };
           
+    }
+    Click = () => {
+        var count = this.state.count;
+        count += 1;
+        this.setState({count : count});
+    }
+    Click2 = () => {
+        var count = this.state.count;
+        count -= 1;
+        this.setState({count : count});
+    }
+    Click3 = () => {
+        var count = this.state.count;
+        count = 0;
+        this.setState({count : count});
     }
         render() {
         return (
             <div>
-                <p>{this.state.count}</p>
-                <button onClick={Click}>Click me </button>
+               <center> <p>{this.state.count}</p></center>
+                <button onClick={this.Click}>Click me ++ </button>
+                <button onClick={this.Click2}>Click me --</button>
+                <button onClick={this.Click3}>Click me = 0</button>
+
             </div>
         )
         }
 }
 
-function Click(){
-    this.setState({count : 0});
-  
-}
+
 
 render(<Button/>,document.getElementById('root'));
