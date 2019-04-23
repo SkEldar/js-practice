a72f3430443439e7b2c8361cf6b24fc6ef2052e4
Eldar <sahaanonimus10@gmail.com>
2019-04-23 16:49:42 +0600

    tasks

--- verstka-2/src/index.js
+++ verstka-2/src/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
 import "./css/style.css";
-import logo from './img/logo.png';
 import Skills from './Skills';
 import Gallery from './Gallery';
 import Footer from './Footer';
+import Header from './Header';
 
 function Header() {
     return(
     <div>
-        <header className="header">
-            <div className="container">
-                <img src={logo} alt="logo" />
-                <div class="offer">
-                     <h1>Я УЧУ HTML5/CSS3</h1>
-                    <p>И мне это дается легко!</p>
-                 </div>
-            </div>
-            </header>
+            <Header />
             <Skills />
             <Gallery />
             <Footer />
