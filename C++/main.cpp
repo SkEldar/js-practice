052a11a5b9800ece5afa29cea7ec83e1482db0a8
Eldar <sahaanonimus10@gmail.com>
2019-08-04 22:18:26 +0600

    c++

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <vector>
 using namespace std;
 int main()
 {
-    int arr[10];
+    vector<int> num;
     for (int i = 0; i < 10; i++)
     {
-        int random = rand();
-        cin >> random;
-        cout << arr[i];
-    };
 
+        num.push_back(rand() % 20);
+        // cout << num[i] << endl;
+    }
+    int max = num[0];
+    int second_max = num[0];
+    for (int l = 0; l < 10; l++)
+    {
+        if (num[l] > max && num[l] > second_max)
+        {
+            max = num[l];
+            second_max = num[l];
+        }
+    };
+    cout << max << endl;
+    cout << second_max << endl;
     return 0;
 }
--- task.js
+++ task.js
@@ -4,6 +4,11 @@ for (let i = 0; i < 10; i++) {
   let rand = Math.random();
   a.push(rand);
 }
-for (let q = 0; q < a.length; q++) {
-  console.log(a[q]);
-}
+console.log(a);
+// for (let q = 0; q < a.length; q++) {
+//   let max = arr[0];
+//   if (a[q] > max) {
+//     max = a[q];
+//   }
+// }
+console.log(Math.max.apply(null, a));
