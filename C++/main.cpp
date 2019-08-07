42b227bb13ac8c117ff7317fa7b1ab6425c8f5b1
Eldar <sahaanonimus10@gmail.com>
2019-08-07 21:50:11 +0600

    c++,my_verstki/responsive-design

--- C++/main.cpp
+++ C++/main.cpp
@@ -16,12 +16,19 @@ int main()
     int second_max = num[0];
     for (int l = 0; l < 10; l++)
     {
-        if (num[l] > max && num[l] > second_max)
+        if (num[l] > max)
         {
             max = num[l];
-            second_max = num[l];
         }
     };
+    for (int c = 0; c < 10; c++)
+    {
+        if (num[c] > second_max && num[c] < max)
+        {
+            second_max = num[c];
+        }
+        // cout << second_max << endl;
+    }
     cout << max << endl;
     cout << second_max << endl;
     return 0;
