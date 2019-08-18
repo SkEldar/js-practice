0db06ed5e4000af2d9476401310f0d89b3df2a4a
Eldar <sahaanonimus10@gmail.com>
2019-08-18 22:28:18 +0600

    responsive-design

--- my_verstki/responsive-verstka/src/index.js
+++ my_verstki/responsive-verstka/src/index.js
@@ -6,71 +6,28 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <div className="header">
-          <h3>Company name</h3>
-          <ul className="menu">
-            <a href="#">
-              <li>Features</li>
-            </a>
-            <a href="#">
-              <li>Enterprise</li>
-            </a>
-            <a href="#">
-              <li>Support</li>
-            </a>
-            <a href="#">
-              <li>Pricing</li>
-            </a>
-          </ul>
-          <button className="Sign_up">Sign up</button>
-        </div>
-        <div className="Main_content">
-          <div className="container">
-            <div className="text">
-              <h1>Pricing</h1>
-              <p>
-                Quickly build an effective pricing table for your potential
-                customers with this
-              </p>
-
-              <p>
-                Bootstrap example. It's built with default Bootstrap components
-                and utilities
-              </p>
-
-              <p> with little customization.</p>
-            </div>
-            <div className="Prices">
-              <div className="Price">
-                <div className="header">
-                  <p>Free</p>
-                </div>
-                <div className="about">
-                  <h1>$0 / mo</h1>
-                </div>
-              </div>
-              <div className="Price">
-                <div className="header">
-                  <p>Pro / mo</p>
-                </div>
-                <div className="about">
-                  <h1>$15 / mo</h1>
-                </div>
-              </div>
-              <div className="Price">
-                <div className="header">
-                  <p>Enterprise</p>
-                </div>
-                <div className="about">
-                  <h1>$29</h1>/
-                </div>
-              </div>
-            </div>
+        <header>
+          <div className="branding">
+            <h3>Company name</h3>
           </div>
-        </div>
-        <div className="footer">
-          <div className="container" />
-        </div>
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Features</a>
+              </li>
+              <li>
+                <a href="#">Enterprise</a>
+              </li>
+              <li>
+                <a href="#">Support</a>
+              </li>
+              <li>
+                <a href="#">Pricing</a>
+              </li>
+            </ul>
+          </nav>
+          <button className="Sign_up">Sign up</button>
+        </header>
       </div>
     );
   }
