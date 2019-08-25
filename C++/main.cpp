0306dc6eff5e33b1a1ab24bdda51628aaeacbf28
Eldar <sahaanonimus10@gmail.com>
2019-08-25 23:36:17 +0600

    poor

--- C++/main.cpp
+++ C++/main.cpp
@@ -5,31 +5,9 @@
 using namespace std;
 int main()
 {
-    vector<int> num;
-    for (int i = 0; i < 10; i++)
-    {
-
-        num.push_back(rand() % 20);
-        // cout << num[i] << endl;
-    }
-    int max = num[0];
-    int second_max = num[0];
-    for (int l = 0; l < 10; l++)
-    {
-        if (num[l] > max)
-        {
-            max = num[l];
-        }
-    };
-    for (int c = 0; c < 10; c++)
-    {
-        if (num[c] > second_max && num[c] < max)
-        {
-            second_max = num[c];
-        }
-        // cout << second_max << endl;
-    }
-    cout << max << endl;
-    cout << second_max << endl;
+    int a, b;
+    cout << "number?" << endl;
+    cin >> a >> b;
+    cout << "a + b = " << a + b << endl;
     return 0;
 }
---
+++ poor/js/script.js
@@ -0,0 +1,9 @@
+let h1 = document.getElementById("h1");
+let timerId = setInterval(() => (h1.innerHTML = "I'm poor!"), 2000);
+setTimeout(() => {
+  clearInterval(timerId);
+  h1.innerHTML = " ";
+}, 5000);
+let rock = "rock";
+let paper = "paper";
+let sizer = "sizer";
