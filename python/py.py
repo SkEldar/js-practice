eba772d8fd4a7a667ddd09fc61dfd61240afae53
SkEldar <sahaanonimus10@gmail.com>
2020-12-27 23:58:26 +0600

    RNC

--- python/py.py
+++ python/py.py
@@ -1,21 +1,34 @@
-while True:
-    what = input("выбери действие: ")
-    a = float(input())
-    b = float(input())
-
-    if what == "+":
-        c = a+b
-        print(c)
-    elif what == "-":
-        c = a-b
-        print(c)
-    elif what == "*":
-        c = a*b
-        print(c)
-    elif what == "/":
-        c = a/b
-        print(c)
-    elif what == "exit":
-       break
-    else:
-        print("error")
+1
+a = int(input())
+b = int(input())
+for i in range(a,b):
+    print(i)
+print(b)
+2
+b = 0
+for i in range(10):
+    a = int(input())
+    b += a
+print(b)
+4
+for i in range(9,21):
+    print(i ** 2)
+5
+a = int(input())
+for i in range(1,11):
+    print(a * i)
+6
+b = int(input())
+a = 0
+for i in range(1,20):
+    print(a + i)
+print(a / b)
+7
+b = int(input())
+8
+b = int(input())
+a = 0
+for i in range(1,b):
+    if i % 2 != 0:
+        a += i
+print(a)
